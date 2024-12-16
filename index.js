c19a646a9d69716a07cce77fc6177b226d56d37d
Mihail Denisenko <i@mihail-denisenko.ru>
2024-12-16 13:35:36 +0300

    new-commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const created = document.querySelector('.preCreated')
+const autorize = document.querySelector(".preAutorize");
+const getUser = document.querySelector(".preUser");
+let classOfPre;
+
+
+// Слушатель кнопок 
+document.addEventListener('click', function (event) {
+    taRget = event.target
+    console.log(taRget.classList)
+    if ((event.target.dataset.postAutorize == "")) {
+        classOfPre = ".preAutorize";
+        event.target.style.transform = "rotate(180deg)"
+    } if (event.target.dataset.getUser == "") {
+        classOfPre = ".preUser";
+        event.target.style.transform = "rotate(180deg)";
+    } if (event.target.dataset.postCreate == "") {
+        event.target.style.transform = "rotate(180deg)";
+        classOfPre = '.preCreated'
+    }
+    classEditor(classOfPre, taRget)
+})
+
+
+
+// функция обработчик кнопок
+function classEditor(classOf, taRget) {
+    taRget.classList.toggle('down__img')
+    taRget.classList.toggle("up__img");
+    console.log(taRget.classList)
+    
+    const elemtnPre = document.querySelector(classOf);
+    console.log(elemtnPre)
+    elemtnPre.classList.toggle('hidden')
+}
+
+
